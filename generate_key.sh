666671418bb11b6550f97bbab235d37b8a69efdd
Pieter Vandepitte <pieter.vandepitte@dnsbelgium.be>
2024-10-11 11:27:55 +0200

    save script

---
+++ generate_key.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+TMP_DIR=$(mktemp -d)
+
+CURRENT_DIR=$(pwd)
+
+cd "$TMP_DIR"
+
+curl -sSL "https://raw.githubusercontent.com/vandepitte/dh/refs/heads/main/dhparam.pem" | openssl genpkey -paramfile /dev/stdin -out private.pem
+
+openssl pkey -in private.pem -pubout -out public.pem
+
+echo "Your API key: \n"
+
+curl -sSL "https://raw.githubusercontent.com/vandepitte/dh/refs/heads/main/public.pem" | openssl pkeyutl -derive -inkey private.pem -peerkey /dev/stdin | base64
+
+echo
+
+echo "Send next key to DNS Belgium:\n"
+
+cat public.pem
+
+cd "$CURRENT_DIR"
+
+rm -rf "$TMP_DIR"
